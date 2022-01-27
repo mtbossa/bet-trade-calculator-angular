ab7282668c432c1ca2779a9b3bc016cbff484403
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-01-27 08:33:00 -0300

    Removed AuthService from CoreModule providers since it's not needed anymore.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [AuthService],
+  providers: [],
 })
 export class CoreModule {}
