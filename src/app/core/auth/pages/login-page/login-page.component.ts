fe3dbe20b249c45130cd5ec1bb33f2aa758ebc84
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-02-02 11:22:01 -0300

    create laravel error validation message

--- src/app/core/auth/pages/login-page/login-page.component.ts
+++ src/app/core/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,16 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/services/auth.service';
+
+interface LoginErrors<T> {
+  message?: string;
+  errors?: T;
+}
+
+type LoginFormErrors = { email?: Array<string>; password?: Array<string> };
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +19,7 @@ import { AuthService } from 'src/app/core/services/auth.service';
 })
 export class LoginPageComponent implements OnInit {
   form!: FormGroup;
-  errors = {};
+  errors: LoginErrors<LoginFormErrors> = {};
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,9 +41,25 @@ export class LoginPageComponent implements OnInit {
           .storeUser()
           .subscribe(() => this.router.navigate(['/dashboard']));
       },
-      error: (err) => {
-        this.errors = err.error.errors;
+      error: (errorResponse: HttpErrorResponse) => {
+        this.errors = {};
+        this.errors = { ...errorResponse.error };
       },
     });
+  }
+
+  getError(property: keyof LoginFormErrors) {
+    if (this.errors.errors && this.errors.errors[property]) {
+      const fieldErrorArray = this.errors.errors[property];
+      if (fieldErrorArray) {
+        return fieldErrorArray[0];
+      }
+    }
+
+    if (this.errors.message) {
+      return this.errors.message;
+    }
+
+    return `Undefined error in ${property} field`;
   }
 }
