225cc75edbc1a2aba07ff4ed837cee7b93bc804f
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-02-02 08:58:16 -0300

    Create login and register page layouts with Angular Material

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -10,7 +12,14 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
 
 @NgModule({
   declarations: [LoginPageComponent, LogoutComponent, RegisterPageComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   exports: [],
 })
 export class AuthModule {}
