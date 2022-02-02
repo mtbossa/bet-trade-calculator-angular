b066dd3a4361c4297769312b94c7c46a91546a5a
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-02-02 19:13:32 -0300

    delete test module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,25 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
-import { DashboardTestPageComponent } from './pages/dashboard-test-page/dashboard-test-page.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
-  declarations: [DashboardPageComponent, DashboardTestPageComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
-  ],
+  declarations: [DashboardPageComponent],
+  imports: [CommonModule, RouterModule, MatGridListModule, LayoutModule],
 })
 export class DashboardModule {}
